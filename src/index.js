a626ebc1cefc1262fc8cd07e4cfb5482e9b207d6
Tym-js <42276835+Tym-js@users.noreply.github.com>
2018-12-09 10:15:09 +0900

    create a register form

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./components/App"
+import Login from "./components/Auth/Login"
+import Register from "./components/Auth/Register"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
-import "./styles.css";
+import "semantic-ui-css/semantic.min.css"
+import "./styles.css"
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+    </Switch>
+  </Router>
+)
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById("root")
+ReactDOM.render(<Root />, rootElement)
