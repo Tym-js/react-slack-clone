2471d08f062761992e6e95b1a743acf49decc74d
Tym-js <42276835+Tym-js@users.noreply.github.com>
2018-12-11 22:08:18 +0900

    scaffold app components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,22 @@
 import React from "react"
+import { Grid } from "semantic-ui-react"
 import "./App.css"
+import ColorPanel from "./ColorPanel/ColorPanel"
+import SidePanel from "./SidePanel/SidePanel"
+import Messages from "./Messages/Messages"
+import MetaPanel from "./MetaPanel/MetaPanel"
 
-const App = () => <h1>React Redux</h1>
+const App = () => (
+  <Grid columns="equal" className="app" style={{ background: "#eee" }}>
+    <ColorPanel />
+    <SidePanel />
+    <Grid.Column style={{ marginLeft: 320 }}>
+      <Messages />
+    </Grid.Column>
+    <Grid.Column width={4}>
+      <MetaPanel />
+    </Grid.Column>
+  </Grid>
+)
 
 export default App
