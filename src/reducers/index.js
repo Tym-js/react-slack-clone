68e9b66f8ba65a169215693d28749641dbc46014
Tym-js <42276835+Tym-js@users.noreply.github.com>
2018-12-15 18:54:08 +0900

    put channel on global state, add channel_reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
-import * as actionTypes from "../actions/types";
-import { combineReducers } from "redux";
+import * as actionTypes from "../actions/types"
+import { combineReducers } from "redux"
 
 const initialState = {
   currentUser: null,
   isLoading: true
-};
+}
 
 const user_reducder = (state = initialState, action) => {
   switch (action.type) {
@@ -12,19 +12,36 @@ const user_reducder = (state = initialState, action) => {
       return {
         currentUser: action.payload.currentUser,
         isLoading: false
-      };
+      }
     case actionTypes.CLEAR_USER:
       return {
-        ...initialState,
+        ...state,
         isLoading: false
-      };
+      }
     default:
-      return state;
+      return state
   }
-};
+}
+
+const initialChannelState = {
+  currentChannel: null
+}
+
+const channel_reducer = (state = initialChannelState, action) => {
+  switch (action.type) {
+    case actionTypes.SET_CURRENT_CHANNEL:
+      return {
+        ...state,
+        currentChannel: action.payload.currentChannel
+      }
+    default:
+      return state
+  }
+}
 
 const rootReducer = combineReducers({
-  user: user_reducder
-});
+  user: user_reducder,
+  channel: channel_reducer
+})
 
-export default rootReducer;
+export default rootReducer
